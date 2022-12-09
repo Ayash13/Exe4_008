8f219a2df0a4a68966c9171e8501160f0c761ad9
Muhammad Ayash <muhammad.ayash.ft21@mail.umy.ac.id>
2022-12-09 16:03:10 +0700

    Create class stack, then create var ayash,top,maxsize

--- Exe4_008/Program.cs
+++ Exe4_008/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿using System;
+
+namespace Exe4_008
+{
+    class Stack
+    {
+        //create variable
+        private decimal[] ayash;
+        private int top;
+        private int maxSize;
+
+        
+    }
+
+}
